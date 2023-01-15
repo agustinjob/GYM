771fe86b3d26538afefc681828b77cb6f1308564
agusjobmacropay <job.hernandez@macropay.mx>
2023-01-15 00:29:05 -0600

    modificación de bd

--- logic/dao/conexion.php
+++ logic/dao/conexion.php
@@ -3,16 +3,16 @@
 class Conectar {
 
     
-    /* private $servidor = "sql349.main-hosting.eu";
+    private $servidor = "sql349.main-hosting.eu";
     private $usuario = "u585135986_spartagym";
     private $bd = "u585135986_spartagym";
-    private $pass = "Spartagym1"; */
+    private $pass = "Spartagym1"; 
 
-    
+  /*   
     private $servidor = "localhost";
     private $usuario = "root";
     private $bd = "gym";
-    private $pass = "";
+    private $pass = ""; */
 
 
     public function conexion() {
